17a612c91b092fe7cf3d1f9f6999c774017a6b9d
prafful solanki <praffulsolanki00@gmail.com>
2018-05-09 12:20:40 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from flask_cors import CORS
 import urllib
 import re
 
 app = Flask(__name__)
-CORS(app)
+
 @app.route('/')
 def index():
   return "welcome"
